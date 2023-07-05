1aa8d8f075a78a5a6e61d4d1ea1aad040d35f57b
omvhadil <fadil.rohman.327@gmail.com>
2023-07-05 14:26:04 +0700

    library calender

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+// const date = ref(new Date())
+const attributes = ref([
+  {
+    // bar: 'green',
+    // dot: 'red',
+    highlight: 'red',
+    dates: new Date()
+  }
+])
+</script>
+
+<template>
+  <div sclass="">
+    <VCalendar :attributes="attributes" expanded locale="id" />
+    <!-- <VDatePicker v-model="date" mode="time" :timezone="timezone" is24hr hide-time-header /> -->
+  </div>
+</template>
+
+<style scoped></style>
